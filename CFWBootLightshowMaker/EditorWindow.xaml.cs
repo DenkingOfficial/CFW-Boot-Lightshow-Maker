8b0ee772eb669d1dd6a32e75700c3a9befcbd352
Denking <shershnev.andrei@gmail.com>
2020-06-07 06:43:07 +0500

    Solution and Simple sh*tcode interface

---
+++ CFWBootLightshowMaker/EditorWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CFWBootLightshowMaker
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MinimizeEvent(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseEvent(object sender, MouseButtonEventArgs e)
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            this.Close();
+        }
+
+        private void TopDrag(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+            e.Handled = true;
+        }
+
+        private void AboutEvent(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
